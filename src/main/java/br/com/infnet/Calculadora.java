23391a3ef6b9cbc1cfea54d87390ab3f7624b4bf
leonardogloria <contato@leonardogloria.com.br>
2023-11-09 21:52:06 -0300

    Alterando GitIgnore e Logando classe Calculadora.java

--- src/main/java/br/com/infnet/Calculadora.java
+++ src/main/java/br/com/infnet/Calculadora.java
@@ -1,7 +1,13 @@
 package br.com.infnet;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Calculadora {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Calculadora.class);
+
     public int soma(int a, int b){
+        LOGGER.info("Iniciando MEtodo de Soma");
         return a + b;
     }
     public int produto(int a, int b){
